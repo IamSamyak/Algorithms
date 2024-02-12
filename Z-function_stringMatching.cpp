e36acd875acf60aee91b6ce80cfae1a5724a0566
IamSamyak <131391298+IamSamyak@users.noreply.github.com>
2024-02-12 21:13:31 +0530

    Made it simpler

--- Z-function_stringMatching.cpp
+++ Z-function_stringMatching.cpp
@@ -1,30 +1,35 @@
-// It calculates the z array of z function in which i'th index stores maximum prefix matching
-void calculateZ(string &s,vector<int>&z){
-        int left = 0,right = 0;
-        int n = s.size();
-        for(int i=1;i<s.size();i++){
-            if(i>right){
-                // this is not part of prefix so calculate for it
-                left = right = i;
-                while(right < n && s[right-left] == s[right]){
-                    right++;
-                }
-                z[i] = right - left;
-                right--;
-            }else{
-                //It is inside z window
-                int k = i - left;
-                if(z[i] < right - left + 1){
-                    z[i] = z[k];
-                }else{
-                    //if it is inside window and matching value exceeds window
-                    left = i;
-                    while(right < n && s[right-left] == s[right]){
-                    right++;
-                    }
-                    z[i] = right - left;
-                    right--;
-                }
-            }
-        }
-    }
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> zMatch(string s) {
+	vector<int> z(s.size(), 0);
+	int l = 0, r = 0;
+	for (int i = 1; i < s.size(); i++) {
+		if (i < r) {
+			z[i] = z[i - l];
+			// Handle case of z[i] reaching for characters 
+			// beyond what we have seen at r
+			if (i + z[i] > r) {
+				z[i] = r - i;
+			}
+		}
+		
+		// Look for more batch beyond the current calculation
+		while(i + z[i] < s.size() && s[z[i]] == s[i + z[i]]) z[i]++;
+		
+		// Update l, r
+		if (i + z[i] > r) {
+			l = i;
+			r = i + z[i];
+		}
+	}
+	return z;
+}
+
+
+int main() {
+	vector<int> z = zMatch("aba$ababbababaaa");
+	for (auto it: z) cout << it << " ";
+	cout << endl;
+	return 0;
+}
